9745c0a48a07386598eae839ea2be0d854e94b21
lizhi <lizzi@live.cn>
2026-09-08 10:44:56 +0800

    fuxi: evo bringup

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -13,11 +13,26 @@ $(call inherit-product, device/xiaomi/fuxi/device.mk)
 
 # Inherit LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_DISABLE_EPPE := true
+TARGET_OPTIMIZED_DEXOPT := true
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := true
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_DEVICE := fuxi
 PRODUCT_NAME := lineage_fuxi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133C
 PRODUCT_MANUFACTURER := Xiaomi
+
+EVO_BUILD_TYPE := Unofficial
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GMS := true
+WITH_SU := true
+BYPASS_CHARGE_SUPPORTED := true
+
+
+TARGET_INCLUDES_OEM_APP := true
+TARGET_INCLUDES_DolbyVision := true
 
 BUILD_FINGERPRINT := Xiaomi/fuxi/fuxi:16/BP2A.250605.031.A3/OS3.0.2.0.WMCCNXM:user/release-keys
